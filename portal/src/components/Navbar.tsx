2ef69850fe859560dad5cc672aed0dcafc8e09ae
James Nuci <jamesnuci@gmail.com>
2024-10-20 19:18:54 -0700

    feat: include loading state for navbar

--- portal/src/components/Navbar.tsx
+++ portal/src/components/Navbar.tsx
@@ -5,7 +5,11 @@ import { useUser } from '@auth0/nextjs-auth0/client'
 
 export default function Navbar() {
 
-  const { user } = useUser();
+  const { user, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div></div>
+  }
 
   return (
     <header className="bg-white">
