a8fe080ba56f91bf539ac7f863a9a9f3ae69df3d
Yimin Arava <yiminarava29@gmail.com>
2025-07-05 14:09:32 -0700

    feat: resume templates section

--- portal/src/app/reforger/page.tsx
+++ portal/src/app/reforger/page.tsx
@@ -1,13 +1,17 @@
 "use client"
 import HeroCard from "@/components/Home/HeroCard";
+import TemplatesSection from "@/components/Home/Templates/TemplatesSection";
 
 const ReforgerMainPage = () => {
 
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-background">
-        <HeroCard />
+      <div className="flex flex-col items-center justify-center min-h-screen ">
+        <section className="flex flex-col gap-6">
+          <HeroCard />
+          <TemplatesSection />
+        </section>
       </div>
-    );
-  };
+    )
+  }
   
   export default ReforgerMainPage;
