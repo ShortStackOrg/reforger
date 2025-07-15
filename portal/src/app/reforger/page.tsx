a95dc7b8d676955559cf7e1cba51b2e6027c282f
James <jamesnuci@gmail.com>
2025-07-14 20:09:23 -0700

    chore: remove spacing above and below main page container

--- portal/src/app/reforger/page.tsx
+++ portal/src/app/reforger/page.tsx
@@ -5,7 +5,7 @@ import TemplatesSection from "@/components/Home/Templates/TemplatesSection";
 const ReforgerMainPage = () => {
 
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen ">
+      <div className="flex flex-col min-h-screen ">
         <section className="flex flex-col gap-6">
           <HeroCard />
           <TemplatesSection />
